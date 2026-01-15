f72ad9b56a3e8f4e8abe298eb63858bcf780fc6c
Corentin Renard <corentinfox08@gmail.com>
2026-01-15 11:58:57 +0100

    chore: initialize SpawnGuard project with package rename

--- src/main/java/fr/akitain/spawnguard/SpawnGuardPlugin.java
+++ src/main/java/fr/akitain/spawnguard/SpawnGuardPlugin.java
@@ -1,16 +1,16 @@
-package dev.hytalemodding;
+package fr.akitain.spawnguard;
 
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
-import dev.hytalemodding.commands.ExampleCommand;
-import dev.hytalemodding.events.ExampleEvent;
+import fr.akitain.spawnguard.commands.ExampleCommand;
+import fr.akitain.spawnguard.events.ExampleEvent;
 
 import javax.annotation.Nonnull;
 
-public class ExamplePlugin extends JavaPlugin {
+public class SpawnGuardPlugin extends JavaPlugin {
 
-    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+    public SpawnGuardPlugin(@Nonnull JavaPluginInit init) {
         super(init);
     }
 
